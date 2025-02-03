549d95a60c94484e46d13263b2d8c3419be9835a
sasuniii0 <sasuniwijerathne@gmail.com>
2025-02-03 11:06:05 +0530

    first commit

---
+++ PawsAndClawsCareMVC-main/src/main/java/lk/ijse/gdse/pawsandclawscaremvc/entity/ServiceDetails.java
@@ -0,0 +1,15 @@
+package lk.ijse.gdse.pawsandclawscaremvc.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ServiceDetails {
+    private String serviceId;
+    private String resId;
+    private String description;
+
+}
---
+++ PawsAndClawsCareMVC-main/src/main/java/lk/ijse/gdse/pawsandclawscaremvc/view/dtm/PetTm.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse.pawsandclawscaremvc.view.dtm;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class PetTm {
+    private String petId;
+    private String name;
+    private String breed;
+}
